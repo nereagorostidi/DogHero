86a35a20f9ee17a2a8b386dd344f750995c0c28f
Rodrigo Agustin Alfaro Olmos <raalfaro@alumnos.inf.utfsm.cl>
2024-10-08 22:44:25 -0300

    firebase connection

--- src/android/app/src/main/kotlin/com/example/src/MainActivity.kt
+++ src/android/app/src/main/kotlin/com/example/src/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.src
+package com.doghero.src
 
 import io.flutter.embedding.android.FlutterActivity
 
--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+
+
 void main() {
   runApp(const MainApp());
 }
@@ -12,9 +14,10 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text('aaa!'),
         ),
       ),
     );
   }
 }
+
